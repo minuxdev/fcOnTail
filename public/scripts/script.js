29b826a2314c87b487b5202d37ea512fd0543ee6
minuxdev <minux.midi@gmail.com>
2022-04-19 22:07:27 -0700

    first commit

---
+++ public/scripts/script.js
@@ -0,0 +1,12 @@
+
+
+
+function open_menu(){
+    var menu = document.getElementById("drop-menu");
+    menu.style.height = "420px"
+}
+
+function close_menu(){
+    var menu_close = document.getElementById("drop-menu");
+    menu_close.style.height = "0px"
+}
